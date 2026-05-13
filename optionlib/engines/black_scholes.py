Fix calculation

optionlib/engines/black_scholes.py
@@ -20,7 +20,7 @@ class Black_Scholes:
         sigma = option.sigma
         q = option.q
 
-        d1 = (np.log(S/K) + (r - q * 0.5 * sigma * 2)*T) / (sigma * np.sqrt(T))
+        d1 = (np.log(S/K) + (r - q + 0.5 * sigma**2)*T) / (sigma * np.sqrt(T))
         d2 = d1 - sigma * np.sqrt(T)
 
         if option.option_type == "call":
@@ -29,6 +29,3 @@ class Black_Scholes:
             price = K * np.exp(-r * T) * norm.cdf(-d2) - S * np.exp(-q * T) * norm.cdf(-d1)
 
         return price
-    
-
-opt = European(S=100, K=100, T=1, r=0.05, sigma=0.2)