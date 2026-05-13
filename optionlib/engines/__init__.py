c109994d113e896bb08d702d01b45cb26cafb091
fedecarz <federico.carzaniga@gmail.com>
2026-05-13 13:42:06 +0200

    add init engines

---
+++ optionlib/engines/__init__.py
@@ -0,0 +1,4 @@
+from optionlib.engines.black_scholes import Black_Scholes
+from optionlib.engines.monte_carlo import MonteCarlo
+from optionlib.engines.binomial import Binomial
+from optionlib.engines.trinomial import Trinomial
